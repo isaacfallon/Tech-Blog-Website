e88410a0c49345de5633b6f7d5c445000c0cda47
isaacfallon <isaac.fallon@gmail.com>
2024-06-10 11:51:54 +0930

    Added edit/delete functionality for posts made by the user

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,29 +1,22 @@
-// const router = require('express').Router();
-// const { User, Post } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//     try {
-//         const postData = await Post.findAll({
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['user']
-//                 }
-//             ]
-//         })
+router.get('/posts/:id', async (req, res) => {
 
-//         const posts = postData.map((project) => project.get({ plain: true }));
+    try {
+        const postData = await Post.findByPk(req.params.id);
 
-//         res.render('dashboard', {
-//             posts,
-//             // TODO: Add a comment describing the functionality of this property
-//             // Gives handlebars access to the boolean variable from 'req.session.logged_in' to conditionally render whether the user is logged in
-//             // logged_in: req.session.logged_in,
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+        const posts = postData.get({ plain: true });
 
-// module.exports = router;
+        res.render('dashboardPost', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
