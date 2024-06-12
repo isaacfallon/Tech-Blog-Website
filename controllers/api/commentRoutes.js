1fc5173042cf4c0df9b86bb82309d386c7cfc72d
isaacfallon <isaac.fallon@gmail.com>
2024-06-12 13:08:20 +0930

    Changes to comment logic

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -25,6 +25,7 @@ router.post('/', async (req, res) => {
             commentContent: req.body.commentContent,
             post_id: req.body.post_id,
             user_id: req.session.user_id,
+            commentDate: req.body.commentDate,
         });
 
         res.status(200).json(commentData);
